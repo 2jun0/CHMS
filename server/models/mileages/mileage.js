2e1a00f4c3cfa0a33fcfaf43cad7cf71fe0c3af7
2jun0 <soo28819@naver.com>
2019-11-10 17:47:02 +0900

    마일리지 조회  수정
    조회 정렬 순서 변경
    코드 -> 이름 -> 입력일

--- server/models/mileages/mileage.js
+++ server/models/mileages/mileage.js
@@ -113,7 +113,7 @@ const Mileage = mongoose.Schema({
 
     // find mileages & count by user num
     Mileage.statics.findByUserNum = function(user_num, dataIndex) {
-        return this.find({user_num}).sort({ "input_date" : -1 }).sort({"code.code": -1}).skip(dataIndex.start).limit(dataIndex.count)
+        return this.find({user_num}).sort({ "input_date" : -1 }).sort({"user_name": -1}).sort({"code.code": -1}).skip(dataIndex.start).limit(dataIndex.count)
             .populate({path:'code', populate:{path:"minor", populate:{path:'major'}}});
     };
 
@@ -123,7 +123,7 @@ const Mileage = mongoose.Schema({
 
     // find mileages & count with filter
     Mileage.statics.findWithFilter = function(filter, dataIndex) {
-        return this.find(filter).sort({ "input_date" : -1 }).sort({"code.code": -1}).skip(dataIndex.start).limit(dataIndex.count)
+        return this.find(filter).sort({ "input_date" : -1 }).sort({"user_name": -1}).sort({"code.code": -1}).skip(dataIndex.start).limit(dataIndex.count)
             .populate({path:'code', populate:{path:"minor", populate:{path:'major'}}});
     };
 
