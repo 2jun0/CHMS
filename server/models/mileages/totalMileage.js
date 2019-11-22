113b0292d8a6d139fbc68434aadd075f96976f57
2jun0 <soo28819@naver.com>
2019-11-23 01:45:47 +0900

    마일리지 총합 모델 업데이트

--- server/models/mileages/totalMileage.js
+++ server/models/mileages/totalMileage.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MinorMileage = mongoose.Schema({
+const TotalMileage = mongoose.Schema({
     user_num:           { type: Number, required: true, unique: true },
     user_name:          { type: String, required: true },
     year_of_study:      { type: Number, required: true },
@@ -14,4 +14,26 @@ const MinorMileage = mongoose.Schema({
     collection: 'TotalMileage'
   });
 
-module.exports = mongoose.model('TotalMileage', MinorMileage);
+  TotalMileage.statics.create = function () {
+
+  }
+
+  TotalMileage.statics.delScore = function(mileage_code, score) {
+
+  }
+
+  TotalMileage.statics.addScore = function(mileage_code, score) {
+
+  }
+
+  TotalMileage.methods.toCustomObject = function() {
+      return {
+          id: this._id,
+          user_num: this.user_num,
+          user_name: this.user_name,
+          year_of_study: this.year_of_study,
+          //......
+      }
+  }
+
+module.exports = mongoose.model('TotalMileage', TotalMileage);
