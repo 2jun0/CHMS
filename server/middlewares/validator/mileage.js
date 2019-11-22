f8130d2f81925c71a4f338fde4280e50a23ee49a
2jun0 <soo28819@naver.com>
2019-11-22 14:29:35 +0900

    Validation 수정
    빈 문자열도 입력이 아예 없는 것으로 처리
    
    1. 프로젝트 Validation 수정
    2. 마일리지 Validation 수정

--- server/middlewares/validator/mileage.js
+++ server/middlewares/validator/mileage.js
@@ -8,15 +8,15 @@ function getCheckMileageBase(inputs_key) {
 
     checkArray = [
        body(inputs_key+'.input_date').not().exists().withMessage(STR_NOT_ALLOW_ACCESS),
-        body(inputs_key+'.act_date').exists().withMessage('참여일자를 입력해주세요'),
-        body(inputs_key+'.act_date.from').exists().withMessage('참여시작일자를 입력해야 합니다.')
+        body(inputs_key+'.act_date').exists().not().isEmpty().withMessage('참여일자를 입력해주세요'),
+        body(inputs_key+'.act_date.from').exists().not().isEmpty().withMessage('참여시작일자를 입력해야 합니다.')
             .custom((value, { req }) => {
                 if (isNaN(Date.parse(value))) { return false; }
                 else{
                     req.body[inputs_key].act_date.from = new Date(value);
                     return true;
             }}).withMessage('참여 시작일을 입력해주세요!'),//.withMessage('참여시작일자의 입력형식이 잘못되었습니다.'),
-        body(inputs_key+'.act_date.to').exists().withMessage('참여종료일자를 입력해야 합니다.')
+        body(inputs_key+'.act_date.to').exists().not().isEmpty().withMessage('참여종료일자를 입력해야 합니다.')
             .custom((value, { req }) => {
                 if (isNaN(Date.parse(value))) { return false; }
                 else{
@@ -24,7 +24,7 @@ function getCheckMileageBase(inputs_key) {
                     return true;
             }}).withMessage('참여 종료일을 입력해주세요!')//.withMessage('참여종료일자의 입력형식이 잘못되었습니다.')
             .custom((value, { req }) => (req.body[inputs_key].act_date.from.getTime() <= req.body[inputs_key].act_date.to.getTime())).withMessage('발생종료일자는 발생시작일자보다 같거나 늦어야합니다.'),
-        body(inputs_key+'.detail').exists().withMessage('활동상세내역을 입력해주세요!')
+        body(inputs_key+'.detail').exists().not().isEmpty().withMessage('활동상세내역을 입력해주세요!')
             .isString().trim().isLength({ min: 1, max: 100 }).withMessage('활동상세내역은 은 최소 1, 최대 100글자여야 합니다.'),
         body(inputs_key+'.info_photos').not().exists().withMessage(STR_NOT_ALLOW_ACCESS),
         body(inputs_key+'.is_accepted').not().exists().withMessage(STR_NOT_ALLOW_ACCESS),
@@ -38,15 +38,15 @@ function getCheckMileageBase(inputs_key) {
 
 exports.checkMileageInputs = function(inputs_key, showError = true) {
     let checkArray = getCheckMileageBase(inputs_key, showError).concat([
-        body(inputs_key+'.user_num').exists().withMessage('사용자 번호를 입력해주세요!')
+        body(inputs_key+'.user_num').exists().not().isEmpty().withMessage('사용자 번호를 입력해주세요!')
             .isInt().withMessage('사용자 번호는 정수 여야 합니다.'),
-        body(inputs_key+'.user_name').exists().withMessage('사용자 성명을 입력해주세요!')
+        body(inputs_key+'.user_name').exists().not().isEmpty().withMessage('사용자 성명을 입력해주세요!')
             .isString().withMessage('사용자 성명은 문자열이여야 합니다.'),
-        body(inputs_key+'.department').exists().withMessage('전공을 입력해주세요!')
+        body(inputs_key+'.department').exists().not().isEmpty().withMessage('전공을 입력해주세요!')
             .isString().withMessage('전공은 문자열이여야 합니다.'),
-        body(inputs_key+'.year_of_study').exists().withMessage('학년을 입력해주세요')
+        body(inputs_key+'.year_of_study').exists().not().isEmpty().withMessage('학년을 입력해주세요')
 			.isInt({ min: 1, max: 4 }).withMessage('학년은 1과 4사이의 정수여야 합니다.'),
-        body(inputs_key+'.code').exists().withMessage('마일리지 코드값을 입력해주세요!')
+        body(inputs_key+'.code').exists().not().isEmpty().withMessage('마일리지 코드값을 입력해주세요!')
             .isString().withMessage('마일리지를 선택해 주세요!'),
     ])
     
@@ -68,7 +68,7 @@ exports.checkMileageUpdate = function(inputs_key, showError = true) {
 
 exports.checkMileageIsAccepted = function(input_key, showError = true) {
     let checkArray = [
-        body(input_key+'.is_accepted').exists().withMessage("사업단 확인여부를 체크해주세요!")
+        body(input_key+'.is_accepted').exists().not().isEmpty().withMessage("사업단 확인여부를 체크해주세요!")
             .isBoolean().withMessage('사업단 확인 여부는 Boolean 값이여야 합니다.')
     ];
 
