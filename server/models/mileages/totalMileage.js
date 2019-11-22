b7f219bd09d1ad4f14b539275614d233ea0b78f2
2jun0 <soo28819@naver.com>
2019-11-23 01:17:50 +0900

    마일리지 합계 모델 추가

---
+++ server/models/mileages/totalMileage.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const MinorMileage = mongoose.Schema({
+    user_num:           { type: Number, required: true, unique: true },
+    user_year_of_study: { type: Number, required: true },
+    user_name:          { type: String, required: true },
+    mileage_score:      [{
+        code:               { type: mongoose.SchemaTypes.ObjectId, ref: 'Codetype.Mileage', required: true },
+        score:              { type: Number, required: true }
+    }],
+    total_score:        { type: Number, required: true },
+    last_update_date:   { type: Date, required: true }
+}, {
+    collection: 'TotalMileage'
+  });
+
+module.exports = mongoose.model('TotalMileage', MinorMileage);
