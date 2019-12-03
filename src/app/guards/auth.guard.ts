cafd0478e8718c2a80b9ffcdeab641ecb55c3833
jihwan512 <wlghks512@naver.com>
2019-12-03 17:15:08 +0900

    마일리지 입력 기한제한

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -16,7 +16,25 @@ export class AuthGuard implements CanActivate {
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    var deadline = "20191203";
+    var today = new Date();
+    var Today: String;
+    var dd = today.getDate();
+    var mm = (today.getMonth()+1); //January is 0!
+    var yyyy = today.getFullYear();
 
+    Today = yyyy.toString();
+    if(mm<10) {
+      Today = Today+"0";
+    } 
+    Today = Today+mm.toString();
+    if(dd<10) {
+      Today = Today+"0";
+    } 
+    Today = Today+dd.toString();
+  
+    console.log(deadline,"   ", Today);
+    console.log((deadline<Today));
     // 토큰 유효 기간 확인
     if (!this.auth.isAuthenticated()) {
       console.log('invalid token');
@@ -32,19 +50,22 @@ export class AuthGuard implements CanActivate {
     // 사용자 종류 확인
     if(route.data && route.data.userTypes) {
       let userTypes = route.data.userTypes as string[];
-
-      if(userTypes.includes(this.auth.getUserType())) {
-        return true;
+      if(deadline<Today && route.data.isInputMileage){
+        alert('마일리지 입력기간이 아닙니다.');
+        return false
       }else{
-        if(route.data.errorMsg) {
-          notifyError(new Error(route.data.errorMsg), true);
-        }
+        if(userTypes.includes(this.auth.getUserType())) {
+          return true;
+        }else{
+          if(route.data.errorMsg) {
+            notifyError(new Error(route.data.errorMsg), true);
+          }
 
-        return false;
+          return false;
+        }
       }
     }else{
       return true;
     }
   }
-
 }
